16cc4a2c05f4f6f1ff8e1ac737e32faca25c8cc5
Luiz Comiran <luizcomiran@gmail.com>
2019-05-24 11:28:16 -0300

    :rocket: Progresso na RNC

--- config.php
+++ config.php
@@ -13,13 +13,13 @@ define('UP_ABSPATH', ABSPATH . '/views/_uploads');
 define('HOME_URI', 'http://localhost/sspiwork');
 
 // Nome do host da base de dados
-define('HOSTNAME', '');
+define('HOSTNAME', 'localhost');
 
 // Nome do DB
-define('DB_NAME', '');
+define('DB_NAME', 'edelbra');
 
 // Usuário do DB
-define('DB_USER', '');
+define('DB_USER', 'root');
 
 // Senha do DB
 define('DB_PASSWORD', '');
