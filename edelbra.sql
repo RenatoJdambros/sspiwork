4edb4641649030a1e6c870dceac9dc56da89eec0
Renato Dambros <renatodambros@terra.com.br>
2019-06-07 19:18:12 -0300

    :rocket: BD atualizado

--- edelbra.sql
+++ edelbra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 06-Jun-2019 às 13:33
+-- Generation Time: 08-Jun-2019 às 00:15
 -- Versão do servidor: 10.1.39-MariaDB
 -- versão do PHP: 7.1.29
 
@@ -84,7 +84,7 @@ CREATE TABLE `rnc` (
 INSERT INTO `rnc` (`id`, `id_origem`, `id_destino`, `descricao`, `justificativa`, `correcao`, `status`, `data_gerada`, `data_finalizada`, `numero_op`, `sacp`, `cliente_nome`, `cliente_obra`, `cliente_telefone`, `cliente_email`) VALUES
 (19, 19, 21, 'de user A para user C', NULL, NULL, 1, '2019-06-04 12:08:21', NULL, NULL, NULL, '', '', '', ''),
 (20, 20, 21, 'de user B para user C', NULL, NULL, 1, '2019-06-04 12:09:07', NULL, 2, NULL, '', '', '', ''),
-(21, 21, 19, 'de user C para user A', NULL, NULL, 1, '2019-06-04 12:09:49', NULL, 3, NULL, '', '', '', ''),
+(21, 21, 19, 'de user C para user A', NULL, NULL, 3, '2019-06-04 12:09:49', '2019-06-06 12:28:00', 3, NULL, '', '', '', ''),
 (22, 22, 20, '123', 'dasdas', 'dasda', 3, '2019-06-05 20:28:46', '2019-06-05 20:37:19', NULL, NULL, '', '', '', '');
 
 -- --------------------------------------------------------
@@ -97,7 +97,6 @@ CREATE TABLE `sacp` (
   `id` int(11) NOT NULL,
   `setor_origem` int(11) NOT NULL,
   `setor_destino` int(11) NOT NULL,
-  `participantes` varchar(5000) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1',
   `origem` varchar(255) NOT NULL,
   `descricao` varchar(5000) NOT NULL,
@@ -114,19 +113,63 @@ CREATE TABLE `sacp` (
 -- Extraindo dados da tabela `sacp`
 --
 
-INSERT INTO `sacp` (`id`, `setor_origem`, `setor_destino`, `participantes`, `status`, `origem`, `descricao`, `proposito`, `consequencia`, `brainstorming`, `data_gerada`, `data_finalizada`, `numero_op`, `id_rnc`) VALUES
-(1, 7, 3, '14;15', 1, '', 'eeasea', '', '', '', '2019-06-03 22:04:20', '0000-00-00 00:00:00', 3123123, NULL),
-(2, 2, 7, '20;21', 1, '', 'descricao', 'proposito', 'conseq', 'brain', '2019-06-05 20:41:18', '0000-00-00 00:00:00', 123, NULL),
-(3, 2, 7, '23', 1, 'opcao2', '\\', '\\', '\\', '\\', '2019-06-05 21:08:12', NULL, NULL, NULL),
-(4, 2, 2, '23', 1, 'czxczxczxczxczxcxzczx', 'a', 'a', 'a', 'a', '2019-06-05 21:08:31', NULL, NULL, NULL),
-(5, 3, 3, '20', 1, '', '', '', '', '', '2019-06-05 21:13:52', NULL, NULL, NULL),
-(6, 3, 3, '20', 1, '', '', '', '', '', '2019-06-05 21:14:33', NULL, NULL, NULL),
-(7, 7, 7, '23', 1, 'recebida', '', '', '', '', '2019-06-05 21:14:59', NULL, NULL, NULL),
-(8, 2, 3, '20', 1, '', '', '', '', '', '2019-06-05 21:15:25', NULL, NULL, NULL),
-(9, 2, 3, '20', 1, '', '', '', '', '', '2019-06-05 21:16:02', NULL, NULL, NULL),
-(10, 2, 2, '23', 1, 'indicador', '', '', '', '', '2019-06-05 21:16:17', NULL, NULL, NULL),
-(11, 7, 7, '23', 1, 'fdfds', 'fsdfsd', 'fsdfsd', 'fsdfds', 'fsdfsd', '2019-06-05 21:17:23', NULL, NULL, NULL),
-(12, 2, 7, '20;21', 1, 'indicador', 'de user A para user C', 'fsdfsd', 'fsdfsd', 'fsdfsd', '2019-06-05 21:48:31', NULL, 90, 19);
+INSERT INTO `sacp` (`id`, `setor_origem`, `setor_destino`, `status`, `origem`, `descricao`, `proposito`, `consequencia`, `brainstorming`, `data_gerada`, `data_finalizada`, `numero_op`, `id_rnc`) VALUES
+(1, 7, 3, 3, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '2019-06-03 22:04:20', '2019-06-06 21:58:42', 12345, NULL),
+(2, 2, 7, 1, '', 'descricao', 'proposito', 'conseq', 'brain', '2019-06-05 20:41:18', '0000-00-00 00:00:00', 123, NULL),
+(3, 2, 7, 3, 'opcao2', '\\', '\\', '\\', '\\', '2019-06-05 21:08:12', '2019-06-06 22:07:41', NULL, NULL),
+(4, 2, 2, 1, 'czxczxczxczxczxcxzczx', 'a', 'a', 'a', 'a', '2019-06-05 21:08:31', NULL, NULL, NULL),
+(5, 3, 3, 1, '', '', '', '', '', '2019-06-05 21:13:52', NULL, NULL, NULL),
+(6, 3, 3, 1, '', '', '', '', '', '2019-06-05 21:14:33', NULL, NULL, NULL),
+(7, 7, 7, 1, 'recebida', '', '', '', '', '2019-06-05 21:14:59', NULL, NULL, NULL),
+(8, 2, 3, 1, '', '', '', '', '', '2019-06-05 21:15:25', NULL, NULL, NULL),
+(9, 2, 3, 1, '', '', '', '', '', '2019-06-05 21:16:02', NULL, NULL, NULL),
+(10, 2, 2, 1, 'indicador', '', '', '', '', '2019-06-05 21:16:17', NULL, NULL, NULL),
+(11, 7, 7, 1, 'fdfds', 'fsdfsd', 'fsdfsd', 'fsdfds', 'fsdfsd', '2019-06-05 21:17:23', NULL, NULL, NULL),
+(12, 2, 7, 1, 'indicador', 'de user A para user C', 'fsdfsd', 'fsdfsd', 'fsdfsd', '2019-06-05 21:48:31', NULL, 90, 19),
+(16, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(17, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(18, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(19, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(20, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(21, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(22, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(23, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(24, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(25, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(26, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(27, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(28, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(29, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(30, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(31, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
+(32, 2, 3, 2, 'relatorio', 'dsada', 'dasdas', 'dasdas', 'dasdas', '0000-00-00 00:00:00', NULL, NULL, NULL),
+(33, 2, 3, 1, 'relatorio', 'DASD', 'DASDAS', 'DASDAS', 'DASDAS', '2019-06-06 21:59:54', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sacp_participantes`
+--
+
+CREATE TABLE `sacp_participantes` (
+  `id` int(11) NOT NULL,
+  `id_sacp` int(11) NOT NULL,
+  `id_participante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `sacp_participantes`
+--
+
+INSERT INTO `sacp_participantes` (`id`, `id_sacp`, `id_participante`) VALUES
+(37, 1, 19),
+(38, 1, 23),
+(39, 1, 25),
+(40, 1, 21),
+(41, 8, 23),
+(42, 8, 21),
+(43, 33, 25),
+(44, 33, 24);
 
 -- --------------------------------------------------------
 
@@ -240,11 +283,13 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `setor`, `email`, `usuario`, `senha`, `tipo_usuario`, `user_session_id`) VALUES
-(19, 'usuário A', 2, 'usuarioA@edelbra.com.br', 'usuarioA', '$2a$08$dMoCrtIiDZl4KWU1H3PI1e2ccGF9j2PFYoTTds5nPpN8XaKsi1gka', 3, '52ipd3p28pgef5po4jahpv3uo4'),
-(20, 'usuário B', 6, 'usuarioB@edelbra.com.br', 'usuarioB', '$2a$08$aJFwqAflqJahcDAObDPis.pxwH/dUIr73FUh29vX6GPH4o9Hseqf2', 3, 'he1qami6nh1oldjui8u3g723qc'),
+(19, 'usuário A', 2, 'usuarioA@edelbra.com.br', 'usuarioA', '$2a$08$dMoCrtIiDZl4KWU1H3PI1e2ccGF9j2PFYoTTds5nPpN8XaKsi1gka', 3, 'j1dvpb03ti22d9umpac3itbl38'),
+(20, 'usuário B', 6, 'usuarioB@edelbra.com.br', 'usuarioB', '$2a$08$aJFwqAflqJahcDAObDPis.pxwH/dUIr73FUh29vX6GPH4o9Hseqf2', 3, 'moo6rofuvkksv197qifti1is0c'),
 (21, 'usuário C', 5, 'usuarioC@edelbra.com.br', 'usuarioC', '$2a$08$BtEveze.8u3z7fCzXs13Gu8TpCMGDUji26i02xzxsW3MnV31h.UEy', 3, '7nsqcpgfr12ih9tens9qakg8ei'),
-(22, 'Administrador', 2, 'administrador@edelbra.com.br', 'admin', '$2a$08$vfXLeWf8sxOPX36Ioo0tX.PQlpSAtegW2MdvngEY88UElveO4GDs2', 1, '7i5kunj24qvds6vsv9gmkhb59l'),
-(23, 'qualidade user', 2, 'qualidade@edelbra.combr', 'qualidade', '$2a$08$FFtkqoNn0p.U4R5FJ3nLPOfas66V57LZGi7/wa3XYt4IDknBVPQKC', 2, 'roq48lrs23pofnnrt34p7865aq');
+(22, 'Administrador', 2, 'administrador@edelbra.com.br', 'admin', '$2a$08$vfXLeWf8sxOPX36Ioo0tX.PQlpSAtegW2MdvngEY88UElveO4GDs2', 1, '63gv81gtqbv3k8lnnprilt9609'),
+(23, 'Qualidade', 2, 'qualidade@edelbra.combr', 'qualidade', '$2a$08$FFtkqoNn0p.U4R5FJ3nLPOfas66V57LZGi7/wa3XYt4IDknBVPQKC', 2, '23h7p2h9721bl7fc29p51ne29e'),
+(24, 'usuário D', 5, 'usuarioD@edelbra.com.br', 'usuarioD', '$2a$08$kSOs1zk3Ax5O.yr96GWNFOQtmxdBvcm7SF4cHGMwqk4ZVHErf.44q', 3, NULL),
+(25, 'maria aparecida da silva sauro teu cu ', 7, 'ghjc2@zdv', 'maria', '$2a$08$s19dKGMjrQMOY9ufiLopq.TQ1AgGZjyZuGiQ8YuFc5jMQ62cOqFoW', 3, 'haertdnpru1as1md5tcbiqtnio');
 
 --
 -- Indexes for dumped tables
@@ -287,6 +332,14 @@ ALTER TABLE `sacp`
   ADD KEY `setor_destino` (`setor_destino`),
   ADD KEY `id_rnc` (`id_rnc`),
   ADD KEY `fk_sacp_status` (`status`);
+
+--
+-- Indexes for table `sacp_participantes`
+--
+ALTER TABLE `sacp_participantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sacp` (`id_sacp`),
+  ADD KEY `id_participante` (`id_participante`);
 
 --
 -- Indexes for table `setores`
@@ -347,7 +400,13 @@ ALTER TABLE `rnc`
 -- AUTO_INCREMENT for table `sacp`
 --
 ALTER TABLE `sacp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `sacp_participantes`
+--
+ALTER TABLE `sacp_participantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `setores`
@@ -377,7 +436,7 @@ ALTER TABLE `tipo_plano_acao`
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Constraints for dumped tables
@@ -415,6 +474,13 @@ ALTER TABLE `sacp`
   ADD CONSTRAINT `fk_sacp_status` FOREIGN KEY (`status`) REFERENCES `status` (`id`),
   ADD CONSTRAINT `fk_setor_destino` FOREIGN KEY (`setor_destino`) REFERENCES `setores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_setor_origem` FOREIGN KEY (`setor_origem`) REFERENCES `setores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `sacp_participantes`
+--
+ALTER TABLE `sacp_participantes`
+  ADD CONSTRAINT `fk_id_participantes` FOREIGN KEY (`id_participante`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `fk_id_sacp_participantes` FOREIGN KEY (`id_sacp`) REFERENCES `sacp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limitadores para a tabela `setores`
