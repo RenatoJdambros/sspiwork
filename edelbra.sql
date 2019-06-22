3b19e4fa40fcbdc1ae8d86774666c35542a31270
Renato <renatodambros@terra.com.br>
2019-06-22 17:23:37 -0300

    SQL

--- edelbra.sql
+++ edelbra.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Jun-2019 às 00:15
--- Versão do servidor: 10.1.39-MariaDB
--- versão do PHP: 7.1.29
+-- Tempo de geração: 22-Jun-2019 às 21:52
+-- Versão do servidor: 10.3.15-MariaDB
+-- versão do PHP: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `edelbra`
+-- Banco de dados: `edelbra`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +34,13 @@ CREATE TABLE `espinha_peixe` (
   `id_tipo_plano_acao` int(11) NOT NULL,
   `descricao` varchar(5000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `espinha_peixe`
+--
+
+INSERT INTO `espinha_peixe` (`id`, `id_sacp`, `id_tipo_plano_acao`, `descricao`) VALUES
+(27, 36, 7, '');
 
 -- --------------------------------------------------------
 
@@ -66,7 +73,7 @@ CREATE TABLE `rnc` (
   `descricao` varchar(5000) NOT NULL,
   `justificativa` varchar(5000) DEFAULT NULL,
   `correcao` varchar(5000) DEFAULT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(11) NOT NULL DEFAULT 1,
   `data_gerada` datetime NOT NULL,
   `data_finalizada` datetime DEFAULT NULL,
   `numero_op` int(11) DEFAULT NULL,
@@ -82,10 +89,11 @@ CREATE TABLE `rnc` (
 --
 
 INSERT INTO `rnc` (`id`, `id_origem`, `id_destino`, `descricao`, `justificativa`, `correcao`, `status`, `data_gerada`, `data_finalizada`, `numero_op`, `sacp`, `cliente_nome`, `cliente_obra`, `cliente_telefone`, `cliente_email`) VALUES
-(19, 19, 21, 'de user A para user C', NULL, NULL, 1, '2019-06-04 12:08:21', NULL, NULL, NULL, '', '', '', ''),
+(19, 19, 21, 'de user A para user C', NULL, NULL, 3, '2019-06-04 12:08:21', '2019-06-19 20:16:21', NULL, NULL, '', '', '', ''),
 (20, 20, 21, 'de user B para user C', NULL, NULL, 1, '2019-06-04 12:09:07', NULL, 2, NULL, '', '', '', ''),
 (21, 21, 19, 'de user C para user A', NULL, NULL, 3, '2019-06-04 12:09:49', '2019-06-06 12:28:00', 3, NULL, '', '', '', ''),
-(22, 22, 20, '123', 'dasdas', 'dasda', 3, '2019-06-05 20:28:46', '2019-06-05 20:37:19', NULL, NULL, '', '', '', '');
+(22, 22, 20, '123', 'dasdas', 'dasda', 3, '2019-06-05 20:28:46', '2019-06-05 20:37:19', NULL, NULL, '', '', '', ''),
+(23, 22, 23, 'dadas', NULL, NULL, 1, '2019-06-19 20:05:07', NULL, NULL, NULL, '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -97,7 +105,7 @@ CREATE TABLE `sacp` (
   `id` int(11) NOT NULL,
   `setor_origem` int(11) NOT NULL,
   `setor_destino` int(11) NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
+  `status` int(11) NOT NULL DEFAULT 1,
   `origem` varchar(255) NOT NULL,
   `descricao` varchar(5000) NOT NULL,
   `proposito` varchar(5000) NOT NULL,
@@ -114,36 +122,7 @@ CREATE TABLE `sacp` (
 --
 
 INSERT INTO `sacp` (`id`, `setor_origem`, `setor_destino`, `status`, `origem`, `descricao`, `proposito`, `consequencia`, `brainstorming`, `data_gerada`, `data_finalizada`, `numero_op`, `id_rnc`) VALUES
-(1, 7, 3, 3, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '2019-06-03 22:04:20', '2019-06-06 21:58:42', 12345, NULL),
-(2, 2, 7, 1, '', 'descricao', 'proposito', 'conseq', 'brain', '2019-06-05 20:41:18', '0000-00-00 00:00:00', 123, NULL),
-(3, 2, 7, 3, 'opcao2', '\\', '\\', '\\', '\\', '2019-06-05 21:08:12', '2019-06-06 22:07:41', NULL, NULL),
-(4, 2, 2, 1, 'czxczxczxczxczxcxzczx', 'a', 'a', 'a', 'a', '2019-06-05 21:08:31', NULL, NULL, NULL),
-(5, 3, 3, 1, '', '', '', '', '', '2019-06-05 21:13:52', NULL, NULL, NULL),
-(6, 3, 3, 1, '', '', '', '', '', '2019-06-05 21:14:33', NULL, NULL, NULL),
-(7, 7, 7, 1, 'recebida', '', '', '', '', '2019-06-05 21:14:59', NULL, NULL, NULL),
-(8, 2, 3, 1, '', '', '', '', '', '2019-06-05 21:15:25', NULL, NULL, NULL),
-(9, 2, 3, 1, '', '', '', '', '', '2019-06-05 21:16:02', NULL, NULL, NULL),
-(10, 2, 2, 1, 'indicador', '', '', '', '', '2019-06-05 21:16:17', NULL, NULL, NULL),
-(11, 7, 7, 1, 'fdfds', 'fsdfsd', 'fsdfsd', 'fsdfds', 'fsdfsd', '2019-06-05 21:17:23', NULL, NULL, NULL),
-(12, 2, 7, 1, 'indicador', 'de user A para user C', 'fsdfsd', 'fsdfsd', 'fsdfsd', '2019-06-05 21:48:31', NULL, 90, 19),
-(16, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(17, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(18, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(19, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(20, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(21, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(22, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(23, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(24, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(25, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(26, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(27, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(28, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(29, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(30, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(31, 7, 3, 2, 'dsadasdas', 'eeasea', 'a', 'a', 'a', '0000-00-00 00:00:00', NULL, 3123123, NULL),
-(32, 2, 3, 2, 'relatorio', 'dsada', 'dasdas', 'dasdas', 'dasdas', '0000-00-00 00:00:00', NULL, NULL, NULL),
-(33, 2, 3, 1, 'relatorio', 'DASD', 'DASDAS', 'DASDAS', 'DASDAS', '2019-06-06 21:59:54', NULL, NULL, NULL);
+(36, 2, 7, 1, 'relatorio', 'a', 'a', 'a', 'a', '2019-06-19 20:26:54', NULL, 90, NULL);
 
 -- --------------------------------------------------------
 
@@ -162,14 +141,7 @@ CREATE TABLE `sacp_participantes` (
 --
 
 INSERT INTO `sacp_participantes` (`id`, `id_sacp`, `id_participante`) VALUES
-(37, 1, 19),
-(38, 1, 23),
-(39, 1, 25),
-(40, 1, 21),
-(41, 8, 23),
-(42, 8, 21),
-(43, 33, 25),
-(44, 33, 24);
+(48, 36, 21);
 
 -- --------------------------------------------------------
 
@@ -286,17 +258,17 @@ INSERT INTO `usuarios` (`id`, `nome`, `setor`, `email`, `usuario`, `senha`, `tip
 (19, 'usuário A', 2, 'usuarioA@edelbra.com.br', 'usuarioA', '$2a$08$dMoCrtIiDZl4KWU1H3PI1e2ccGF9j2PFYoTTds5nPpN8XaKsi1gka', 3, 'j1dvpb03ti22d9umpac3itbl38'),
 (20, 'usuário B', 6, 'usuarioB@edelbra.com.br', 'usuarioB', '$2a$08$aJFwqAflqJahcDAObDPis.pxwH/dUIr73FUh29vX6GPH4o9Hseqf2', 3, 'moo6rofuvkksv197qifti1is0c'),
 (21, 'usuário C', 5, 'usuarioC@edelbra.com.br', 'usuarioC', '$2a$08$BtEveze.8u3z7fCzXs13Gu8TpCMGDUji26i02xzxsW3MnV31h.UEy', 3, '7nsqcpgfr12ih9tens9qakg8ei'),
-(22, 'Administrador', 2, 'administrador@edelbra.com.br', 'admin', '$2a$08$vfXLeWf8sxOPX36Ioo0tX.PQlpSAtegW2MdvngEY88UElveO4GDs2', 1, '63gv81gtqbv3k8lnnprilt9609'),
+(22, 'Administrador', 2, 'administrador@edelbra.com.br', 'admin', '$2a$08$vfXLeWf8sxOPX36Ioo0tX.PQlpSAtegW2MdvngEY88UElveO4GDs2', 1, 'vc0csgpjvtelraguc3p87ed1kv'),
 (23, 'Qualidade', 2, 'qualidade@edelbra.combr', 'qualidade', '$2a$08$FFtkqoNn0p.U4R5FJ3nLPOfas66V57LZGi7/wa3XYt4IDknBVPQKC', 2, '23h7p2h9721bl7fc29p51ne29e'),
 (24, 'usuário D', 5, 'usuarioD@edelbra.com.br', 'usuarioD', '$2a$08$kSOs1zk3Ax5O.yr96GWNFOQtmxdBvcm7SF4cHGMwqk4ZVHErf.44q', 3, NULL),
 (25, 'maria aparecida da silva sauro teu cu ', 7, 'ghjc2@zdv', 'maria', '$2a$08$s19dKGMjrQMOY9ufiLopq.TQ1AgGZjyZuGiQ8YuFc5jMQ62cOqFoW', 3, 'haertdnpru1as1md5tcbiqtnio');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `espinha_peixe`
+-- Índices para tabela `espinha_peixe`
 --
 ALTER TABLE `espinha_peixe`
   ADD PRIMARY KEY (`id`),
@@ -304,7 +276,7 @@ ALTER TABLE `espinha_peixe`
   ADD KEY `id_tipo_plano_acao` (`id_tipo_plano_acao`);
 
 --
--- Indexes for table `planos_acao`
+-- Índices para tabela `planos_acao`
 --
 ALTER TABLE `planos_acao`
   ADD PRIMARY KEY (`id`),
@@ -315,7 +287,7 @@ ALTER TABLE `planos_acao`
   ADD KEY `status` (`status`);
 
 --
--- Indexes for table `rnc`
+-- Índices para tabela `rnc`
 --
 ALTER TABLE `rnc`
   ADD PRIMARY KEY (`id`),
@@ -324,7 +296,7 @@ ALTER TABLE `rnc`
   ADD KEY `id_destino` (`id_destino`);
 
 --
--- Indexes for table `sacp`
+-- Índices para tabela `sacp`
 --
 ALTER TABLE `sacp`
   ADD PRIMARY KEY (`id`),
@@ -334,7 +306,7 @@ ALTER TABLE `sacp`
   ADD KEY `fk_sacp_status` (`status`);
 
 --
--- Indexes for table `sacp_participantes`
+-- Índices para tabela `sacp_participantes`
 --
 ALTER TABLE `sacp_participantes`
   ADD PRIMARY KEY (`id`),
@@ -342,32 +314,32 @@ ALTER TABLE `sacp_participantes`
   ADD KEY `id_participante` (`id_participante`);
 
 --
--- Indexes for table `setores`
+-- Índices para tabela `setores`
 --
 ALTER TABLE `setores`
   ADD PRIMARY KEY (`id`),
   ADD KEY `responsavel` (`responsavel`);
 
 --
--- Indexes for table `status`
+-- Índices para tabela `status`
 --
 ALTER TABLE `status`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tipos_usuario`
+-- Índices para tabela `tipos_usuario`
 --
 ALTER TABLE `tipos_usuario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tipo_plano_acao`
+-- Índices para tabela `tipo_plano_acao`
 --
 ALTER TABLE `tipo_plano_acao`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `usuarios`
+-- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
@@ -375,79 +347,79 @@ ALTER TABLE `usuarios`
   ADD KEY `setor` (`setor`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `espinha_peixe`
+-- AUTO_INCREMENT de tabela `espinha_peixe`
 --
 ALTER TABLE `espinha_peixe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
--- AUTO_INCREMENT for table `planos_acao`
+-- AUTO_INCREMENT de tabela `planos_acao`
 --
 ALTER TABLE `planos_acao`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `rnc`
+-- AUTO_INCREMENT de tabela `rnc`
 --
 ALTER TABLE `rnc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `sacp`
+-- AUTO_INCREMENT de tabela `sacp`
 --
 ALTER TABLE `sacp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
--- AUTO_INCREMENT for table `sacp_participantes`
+-- AUTO_INCREMENT de tabela `sacp_participantes`
 --
 ALTER TABLE `sacp_participantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
--- AUTO_INCREMENT for table `setores`
+-- AUTO_INCREMENT de tabela `setores`
 --
 ALTER TABLE `setores`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `status`
+-- AUTO_INCREMENT de tabela `status`
 --
 ALTER TABLE `status`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tipos_usuario`
+-- AUTO_INCREMENT de tabela `tipos_usuario`
 --
 ALTER TABLE `tipos_usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `tipo_plano_acao`
+-- AUTO_INCREMENT de tabela `tipo_plano_acao`
 --
 ALTER TABLE `tipo_plano_acao`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- Constraints for dumped tables
+-- Restrições para despejos de tabelas
 --
 
 --
 -- Limitadores para a tabela `espinha_peixe`
 --
 ALTER TABLE `espinha_peixe`
-  ADD CONSTRAINT `fk_sacp_peixe` FOREIGN KEY (`id_sacp`) REFERENCES `sacp` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_tipo_plano_peixe` FOREIGN KEY (`id_tipo_plano_acao`) REFERENCES `tipo_plano_acao` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_sacp_peixe` FOREIGN KEY (`id_sacp`) REFERENCES `sacp` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_tipo_plano_peixe` FOREIGN KEY (`id_tipo_plano_acao`) REFERENCES `tipo_plano_acao` (`id`);
 
 --
 -- Limitadores para a tabela `planos_acao`
